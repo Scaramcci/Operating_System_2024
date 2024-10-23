63e6b65024731df3ec4c4892bb76f37101899a7a
Xing-Qinghua <157476440+1Reminding@users.noreply.github.com>
2024-10-23 16:03:57 +0800

    SLUB

---
+++ lab2/slub_pmm/slub_pmm.h
@@ -0,0 +1,10 @@
+#ifndef __KERN_MM_SLUB_FIT_PMM_H__
+#define __KERN_MM_SLUB_FIT_PMM_H__
+
+#include <pmm.h>
+
+// 声明 SLUB 分配算法的 PMM 管理器
+extern const struct pmm_manager slub_pmm_manager;
+
+#endif /* ! __KERN_MM_SLUB_FIT_PMM_H__ */
+
